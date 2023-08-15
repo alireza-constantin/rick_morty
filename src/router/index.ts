c4afb9a84f0b5669c4231ed67f4e1ac5ae449475
alireza <alirezasoheili1224@yahoo.com>
2023-08-15 16:14:43 +0330

    add routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,22 +2,38 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            name: "home",
+            component: HomeView,
+        },
+        {
+            path: "/characters",
+            name: "characters",
+            // route level code-splitting
+            // this generates a separate chunk (About.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import("../views/CharactersView.vue"),
+        },
+        {
+            path: "/episodes",
+            name: "episodes",
+            // route level code-splitting
+            // this generates a separate chunk (About.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import("../views/EpisodesView.vue"),
+        },
+        {
+            path: "/locations",
+            name: "locations",
+            // route level code-splitting
+            // this generates a separate chunk (About.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import("../views/LocationsView.vue"),
+        },
+    ],
 })
 
 export default router
